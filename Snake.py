3e291daebb67c881a26ccdbfc7eeefd543ba2a83
DanSalib <dansalib@outlook.com>
2017-01-04 23:22:30 -0500

    Creating outputs to console

--- Snake.py
+++ Snake.py
@@ -3,11 +3,6 @@ import time
 from collections import namedtuple
 
 
-#if __name__ == '__main__':
- #   wrapper(main)
-    # Wrapper does the setup and then calls main
-    # after main is done, it puts back everything as it was
-
 Pos = namedtuple('Pos', ['x', 'y'])
 
 class IllegalMove(Exception):
@@ -25,32 +20,27 @@ class Snake:
         
         self.grow_at = []
         
+    def turn_south(self):
+        if self.cdir == 'n':
+            raise IllegalMove("Can't move from south to north")
+        else:
+            cdir = 's'
 
     def turn_north(self):
         if self.cdir == 's':
-            self.cdir = 's'
             raise IllegalMove("Can't move from south to north")
         else:
-            self.cdir = 'n'
-        
-    def turn_south(self):
-        if self.cdir == 'n':
-            self.cdir == 'n'
-            raise IllegalMove("Can't move from north to south")
-        else:
-            cdir = 's'
+            self.cdir = 'n'        
         
     def turn_east(self):
         if self.cdir == 'w':
-            self.cdir == 'w'
-            raise IllegalMove("Can't move from north to south")
+            raise IllegalMove("Can't move from west to east")
         else:
             cdir = 'e'
                            
     def turn_west(self):
         if self.cdir == 'e':
-            self.cdir == 'e'
-            raise IllegalMove("Can't move from north to south")
+            raise IllegalMove("Can't move from east to west")
         else:
             cdir = 'w'
 
@@ -59,6 +49,10 @@ class Snake:
     
 
     def move_forward(self):
+        '''
+Move forward by one step.
+Raises a GameLost exception if the snake coils on itself or if it hits the edge
+    '''
         # Check for two things
         #   1. Did the snake hit itself
         #   2. Did the snake it one of the borders?
@@ -66,12 +60,12 @@ class Snake:
         # Use array.index
         head = self.posArr[0]
         if self.cdir == 'n':
-            newPos = Pos(x = head.x, y = head.y + 1)
-        elif self.cdir == 's':
             newPos = Pos(x = head.x, y = head.y - 1)
-        elif self.cdir == 'e':
+        if self.cdir == 's':
+            newPos = Pos(x = head.x, y = head.y + 1)
+        if self.cdir == 'e':
             newPos = Pos(x = head.x + 1, y = head.y)
-        elif self.cdir == 'w':
+        if self.cdir == 'w':
             newPos = Pos(x = head.x - 1, y = head.y)
         
         try:
@@ -87,47 +81,66 @@ class Snake:
         if newPos.y <= -1 or newPos.y >= self.LENGTH:
             raise GameLost('You hit the border')
 
-        
-        # INSERT newPos
         self.posArr.insert(0, newPos)
         
-        if not (self.grow_at is None or self.posArr[-1] != self.grow_at):
-            pop.self.grow_at[-1]
+        if len(self.grow_at) > 0 and self.posArr[0] ==  self.grow_at[0]:
+            self.grow_at.pop(0)
         else:
             self.posArr.pop()
+
+    def draw(self, stdscr):
+        ''' Draw the snake on stdscr '''
+
+        # IMPLEMENT
+    
+        # USE
+        for pos in self.posArr:
+            stdscr.addstr(pos.y, pos.x, '~')
 
 def randomApple(applex, appley):
     applex = randint(0, WIDTH)
     appley = randint(0, LENGTH)
-    
-LENGTH = 8
-WIDTH = 12
-applex = 8
-appley = 8
 
-snake = Snake(LENGTH, WIDTH)
+def main(stdscr):   
+    LENGTH = 8
+    WIDTH = 12
+    applex = 8
+    appley = 8
 
-while(True):
-    while(snake.posArr[0] != Pos(x = applex, y = appley)):
-        try:
-          snake.move_forward()
-        except ValueError or GameLost():
-          break
-    if(KEY_UP):
-          snake.turn_north()
-    elif(KEY_DOWN):
-          snake.turn_south()
-    elif(KEY_LEFT):
-          snake.turn_west()
-    elif(KEY_RIGHT):
-          snake.turn_east()
-    if(snake.posArr[0] == Pos(x = applex, y = appley)):
-          snake.grow()
-          randomApple(applex, appley)
-    
+    snake = Snake(LENGTH, WIDTH)
+    #snake.turn_south()
+    #snake.turn_north()
+    #snake.turn_east()
+    #snake.turn_west()
 
+    while True:
+        while True: #(stdscr.getkey() == or snake.posArr[0] != Pos(x=applex, y=appley)):
+            try:
+              snake.move_forward()
+              #print and delay
+              stdscr.clear()
+              snake.draw(stdscr)
+              stdscr.refresh()
+              time.sleep(0.5)
+            except ValueError or GameLost():
+              break
+        if(stdscr.getkey() == KEY_UP):
+              snake.turn_north()
+        elif(stdscr.getkey() == KEY_DOWN):
+              snake.turn_south()
+        elif(stdscr.getkey() == KEY_LEFT):
+              snake.turn_west()
+        elif(stdscr.getkey() == KEY_RIGHT):
+              snake.turn_east()
+        if(snake.posArr[0] == Pos(x = applex, y = appley)):
+              snake.grow()
+              randomApple(applex, appley)
+        
 
-
+if __name__ == '__main__':
+   wrapper(main)
+    # Wrapper does the setup and then calls main
+    # after main is done, it puts back everything as it was
 
         
 
